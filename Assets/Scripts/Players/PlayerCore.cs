9ca1b92d6fd0f8a72e542a2b9bf2589a8190a699
Shota HAMADA <maku693@outlook.com>
2018-03-31 05:20:45 +0900

    Implement IPlayerPosition on PlayerCore

--- Assets/Scripts/Players/PlayerCore.cs
+++ Assets/Scripts/Players/PlayerCore.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 using UniRx;
+using UniRx.Triggers;
 using System;
 
 namespace Players
 {
     [RequireComponent(typeof(PlayerParameters))]
-    public class PlayerCore : MonoBehaviour
+    public class PlayerCore : MonoBehaviour, IPlayerPosition
     {
         [SerializeField] private PlayerParameters _parameters;
         public PlayerParameters PlayerParameters => _parameters;
@@ -13,6 +14,8 @@ namespace Players
         private readonly FloatReactiveProperty _moveSpeed = new FloatReactiveProperty(0);
 
         public IObservable<Vector2> Movement => _moveSpeed.Select(speed => Vector2.up * speed);
+
+        public IObservable<Vector3> Position => this.FixedUpdateAsObservable().Select(_ => transform.position);
 
         private void Awake()
         {
