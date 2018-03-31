e06897e56b7443e9bde8e5d86cfaea425b8489ba
Shoma SATO <noir.neo.04@gmail.com>
2018-04-01 05:05:43 +0900

    White out

--- Assets/Scripts/Result/ResultUi.cs
+++ Assets/Scripts/Result/ResultUi.cs
@@ -1,14 +1,40 @@
-﻿using UnityEngine;
+﻿using Characters;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
 
 namespace Result
 {
     public class ResultUi : MonoBehaviour
     {
         [SerializeField] private Animator _animator;
+        [SerializeField] private Image _bg;
+        [SerializeField] private Transform _goal; // FIXME
 
         public void ShowResult(bool isClear)
         {
+            var color = _bg.color;
+            color.a = 1f;
+            _bg.color = color;
             _animator.SetTrigger(isClear ? "Success" : "Failed");
+        }
+
+        [Inject]
+        void Initialize(ICharacterPosition playerPosition)
+        {
+            var goalPosition = _goal.position;
+            playerPosition.Position.First()
+                .Select(x => (goalPosition - x).sqrMagnitude)
+                .CombineLatest(playerPosition.Position.Select(x => (goalPosition - x).sqrMagnitude),
+                    (max, current) => Mathf.Pow(1 - current / max, 3))
+                .Subscribe(x =>
+                {
+                    var color = _bg.color;
+                    color.a = x;
+                    _bg.color = color;
+                })
+                .AddTo(this);
         }
     }
 }
