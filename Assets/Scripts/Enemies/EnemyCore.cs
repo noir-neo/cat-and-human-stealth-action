e1223beb055b7402884b16fd3295e8583b732463
Shoma SATO <noir.neo.04@gmail.com>
2018-03-31 19:29:02 +0900

    Move forward enemy

---
+++ Assets/Scripts/Enemies/EnemyCore.cs
@@ -0,0 +1,16 @@
+﻿using Characters;
+using UnityEngine;
+
+namespace Enemies
+{
+    [RequireComponent(typeof(CharacterCore))]
+    public class EnemyCore : MonoBehaviour
+    {
+        [SerializeField] private CharacterCore _characterCore;
+
+        void Start()
+        {
+            _characterCore.Direction = Vector2.up;
+        }
+    }
+}
